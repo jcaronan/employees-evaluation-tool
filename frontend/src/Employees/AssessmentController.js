adb149c71f084115fc009140d17d6fde5aafab1e
jcaronan <jasmin.caronan@gmail.com>
2015-11-24 21:28:18 +0800

    Fix the unexpected token error.

--- frontend/src/Employees/AssessmentController.js
+++ frontend/src/Employees/AssessmentController.js
@@ -2,7 +2,7 @@
  * Created by jcaronan on 11/4/15.
  */
 
-import PDFdoc from '../../node_modules/pdfkit'
+//import PDFdoc from '../../node_modules/phantomjs-pdf/lib/phantomjs-pdf'
 
 export class AssessmentController {
   constructor(AssessmentService, AnswerService, questions, EmployeeService) {
@@ -18,28 +18,12 @@ export class AssessmentController {
     return this.employee.name
   }
 
-  getQuestionSet(){
-
-  }
-
   autoSave(value, q) {
     this.AnswerService.addUpdateAnswer(this.AssessmentService.assessmentId,value, q)
   }
 
   generatePdf(){
-    var doc = new jsPDF();
 
-    var specialElementHandlers = {
-      '#editor': function(element, renderer){
-        return true;
-      }
-    };
-
-    doc.fromHTML($('page-wrapper').get(0), 15, 15, {
-      'width': 170,
-      'elementHandlers': specialElementHandlers
-    });
-    doc.save('sample-page.pdf');
   }
 }
 AssessmentController.$inject = ['AssessmentService' , 'AnswerService', 'questions', 'EmployeeService']
